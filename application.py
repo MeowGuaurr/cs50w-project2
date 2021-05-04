307d7f7263fd7cd85fa643ef25ddc6de6719f567
MeowGuaurr <reynitamercedes@gmail.com>
2021-05-04 05:14:39 +0000

    log and channels on process

--- application.py
+++ application.py
@@ -3,19 +3,35 @@ import os
 from flask import Flask, session, render_template, request, redirect
 from flask_socketio import SocketIO, emit, join_room, leave_room
 from flask_session import Session
+from collections import deque
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
 app.config["SESSION_TYPE"] = "filesystem"
 socketio = SocketIO(app)
 
-Session(app)
+channels={"Room": {"msg": deque([], maxlen=100), "username": "def"}}
+users = {}
 
-socketio = SocketIO(app, manage_session=False)
-
-@app.route("/", methods=['GET','POST'])
+@app.route("/")
 def index():
     return render_template('index.html')
+
+@socketio.on('userdata')
+def userdata(data):
+    if 'username' in data:
+        users[data['username']] = request.sid
+
+
+@socketio.on('new')
+def new(data):
+    if data['name'] in channels:
+        return false
+    else:
+        chanels[data['name']]={}
+        channels[data['name']]['msg']= deque(maxlen=100)
+        channels[data['username']]=data['username']
+        emit('new', {"name": data['name']}, broadcast= True)
 
 #@app.route("/chat", methods=['GET','POST'])
 #def chat():
---
+++ static/main.js
@@ -0,0 +1,44 @@
+document.addEventListener("DOMContentLoaded", () => {
+    get_username();
+})
+
+const get_username =() => {
+    let username = localStorage.getItem("username");
+    if(!username){
+         $(".modal").modal({ show: true, backdrop: "static"});
+        document.querySelector("#new-username").addEventListener("submit", e =>{
+            e.preventDefault();
+            username = document.querySelector("#user-name").value;
+            console.log(username);
+
+            if(typeof username == "string"){
+                username = username.trim()
+                if (username == ""){
+                    username = null;
+                }
+                else{
+                    localStorage.setItem("username", username);
+                    $(".modal").modal("hide");
+                    initialize(username);
+                }
+            }
+        });
+    }
+
+    else{
+        initialize(username);
+    }
+
+};
+
+const initialize = username =>{
+    let socket= io.connect(location, protocol + "//" + document.domain + ":" + location.port);
+    socket.on("connect", () =>{
+        socket.emit("userdata", {username});
+        setup(socket);
+
+    socket.on("new", data => {
+        show_channel(data.name, socket);
+    });
+    });
+};
