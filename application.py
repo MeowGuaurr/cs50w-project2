ec4493f61528818de6bc50b51fad09fb766a418f
MeowGuaurr <reynitamercedes@gmail.com>
2021-05-12 08:42:27 +0000

    :white_check_mark: gordito si ves esto, aun no terminox5

--- application.py
+++ application.py
@@ -15,72 +15,47 @@ socketio = SocketIO(app)
 
 
 channelslist=[]
-channelText = dict()
+channels = {}
+channels["General"]=[]
+
 limit=100
-channelText["general"]=[]
-users =[]
+users ={}
+private_messages={}
 
 @app.route("/")
 @log_req
 def index():
-    return render_template('index.html', channelslist = channelslist)
+    return render_template('index.html')
 
-@app.route("/login", methods=['GET','POST'])
-def login():
-    session.clear()
-    username = request.form.get("username")
-    if request.method == "POST":
-        if username in users:
-            return "invalid, username exists"
-        users.append(username)
-        session['username'] = username
-        session.permanent = True
-        return render_template("index.html")
+@socketio.on('connect')
+def connect():
+    emit("load channels", {'channels': channels})
+
+@socketio.on('new username')
+def new_username(data):
+    username=""
+    error=""
+    if data['username'] in users_list:
+        error="Username already exist"
     else:
-        return render_template("login.html")
+        users[data['username']]=request.sid
+        username = data['username']
 
+    emit('add username', {"username": username, "error": error})
 
+@socketio.on('submit to all')
+def message_to_all(data):
+    message = {'text': data["my_message"], 'username':data["username"], 'time':data["time"]}
+    channel['General'].append(message)
 
-@app.route("/new_channel", methods=['GET','POST'])
-def new_channel():
-    newchannel = request.form.get("channel")
-    if newchannel in channelslist:
-        return ("already exists")
-    else:
-        channelslist.append("new_channel")
-        channelText[newchannel] = deque()
-        return redirect("/" + newchannel)
+    if (len(channels['General']) > 100):
+        channels['General'].pop(0)
 
-@socketio.on("text")
-def message(data):
-    time = asctime(localtime())
-    username = session.get('username')
-    channel = session.get('channel')
-    message = data["message"]
-    room = session.get('channel')
-    channelText[channel].append([username, message, time])
-    emit('announce message', {"user": username, "message": message, "time": time}, room = room)
+    emit('message to all', {'channles': channels}, broadcast = True)
 
-
-@socketio.on('join')
-def join():
-    username = session.get('username')
-    room= session.get['channel']
-    join_room(room)
-    emit('joined', {"message": username + "joined"}, room=room)
-
-
-@app.route("/<channel>")
-def channel(channel):
-    session['channel']= channel
-    return render_template('chat.html', channelslist=channelslist, broadcast=True)
-
-@app.route("/logout")
-def log_out():
-    username=session['username']
-
-    session.clear()
-    return redirect("/")
+@socketio.on('return to general')
+def return_to_general(data):
+    emit('announce to all', {'channels': channels}, broadcast = True)
 
 if __name__ == '__main__':
     socketio.run(app)
--- static/main.js
+++ static/main.js
@@ -1,51 +1,84 @@
-document.addEventListener("DOMContentLoaded", () => {
-    get_username();
-})
-    var socket = io.connect(location, protocol + "//" + document.domain + ":" + location.port);
-    socket.on('connect', () => {
+let local = localStorage.getItem("username")
+let Lchannel = localStorage.getItem("last_channel")
+let Cchannel = (Lchannel) ? Lchannel : ""
+let socket = null
+let message_temp = null
+function get_username(){
+    const log_form = document.querySelector()
+    const formUser = document.querySelector()
+    const formSubmit = document.querySelector()
+    formSubmit.disabled = true
 
-       socket.emit('join', {'username' : localStorage.getItem("localuser")});
-        document.querySelector('#submit').disabled = true;
-       document.querySelector('#msg').onkeyup = () => {
-           if(document.querySelector('#msg').value.length < 0)
-                document.queryelector('#submit').disabled = true;
-           else
-                document.querySelector('#submit').disabled = false;
-       };
+    formUser.onkeyup = () => {
+        formSubmit.disabled = (formUser.value.length > 0) ? false : true
+    }
 
-        document.querySelector('#nmsg').onsubmit = () => {
-            const message = document.querySelector('#msg').value;
-            document.querySelector('#msg').value = '';
-            document.querySelector('#submit').disabled = true;
-            socket.emit('text',{'message' : message});
+    formSubmit.onsubmit = () => {
+        username = formUser.value
+        localStorage.getItem('username', username)
+        window.location.reload(false)
+        return false
+    }
 
-            return false;
-        };
+    log_form.style.display = "block"
+}
 
-    });
+function triggerSocket() {
+    const socketServer = io.connect(`${location.protocol} // ${document.domain}:${location.port}`)
+    socketServer.on('connect', () => {
+        document.querySelector().onsubmit = () => {
+            socketSever.emmit("channelsnew", {"channel": document.querySelector().value})
+            document.querySelector().value = ""
+            return false
+        }
+        if(Cchannel){
+            socket.emit("channels-join",{
+                "username": username,
+                "channel": Cchannel
+            })
+        }
+    })
 
-    socket.on('announce message', data => {
-        let l = localStorage.getItem("localuser");
-        console.log(l);
-        const li = document.createElement('li');
-        let usuario = data.user;
-        if (usuario != l){
-            li.className = 'localchat';
+    socketServer.on('add username', data => {
+        if(data.error != ""){
+            window.setTimeout(function(){
+                const FlackModal = document.querySelector("#FlackModal")
+                flackModal.modal({backdrop: 'static', keyboard: false})
+                const modalTitle = document.querySelector('.modal-title')
+                modalTitle.text(data.error)
+                const modalInput = document.querySelector("#modalInput")
+                modalInput.val("")
+                const modalButton = document.querySelector("#modalButton")
+                modalTitle.attr('disabled', true)
+            }, 900)
         }
         else{
-            li.className = 'chat'
+            localStorage.setItem('username', username)
+            const usernameID = document.querySelector("#usernameID")
+            usernameID.text(localStorage.getItem("username"))
+
+            const generalChannel = document.querySelector("#general")
+            generalChannel.click()
+
+            const messageInput = document.querySelector("#messageInput")
+            messageInput.focus()
         }
-        li.innerHTML = `<b>${data.user}: <b>${data.message} / ${data.time}`;
-        document.querySelector('#msg').append(li);
-    });
+    })
+}
 
-    socket.on('joined', data => {
-        const li=document.createElement('li');
-        li.innerHtml =  `<b>${data.message}`;
-        document.querySelector('#msg').append(li);
-    });
+document.addEventListener("DOMContentLoaded", () => {
+    message_temp = Handlebars.compile(document.querySelector().innerHtml)
+    if(!username){
+        get_username();
+    }
+    else{
+        const gUser= document.querySelector('#greet')
+        greet.innerHtml = `Hello, ${username}`
+        greet.style.display = "block"
 
-    document.querySelector('#log').onsubmit =() => {
-        const username = document.querySelector('#username').value;
-        localStorage.setItem("localuser", username);
-    };
+        socket =  triggerSocket()
+
+        document.querySelector().style.display = "block"
+    }
+
+})
